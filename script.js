465bf10b138a3d27719b542a026acb153863f1a2
Leothurm <leonardo.thurm@gmail.com>
2023-01-21 19:13:46 -0300

    JavaScript parte 3 - aviso do voto nulo

--- script.js
+++ script.js
@@ -52,6 +52,11 @@ function atualizaInterface() {
     }
 
     lateral.innerHTML = fotosHtml;
+  } else{
+    seuVotoPara.style.display = "block";
+    aviso.style.display = "block";
+    descricao.innerHTML = '<div class="aviso-grande pisca">VOTO NULO</div>';
+
   }
 }
 
